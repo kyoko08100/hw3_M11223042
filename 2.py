189d0fbc57e286169327cefd2a71416094ee5e73
kyoko08100 <eric22221936@gmail.com>
2024-05-29 14:30:44 +0800

    fix the bug of when a frame has no bounding box break while loop

--- 2.py
+++ 2.py
@@ -1,3 +1,4 @@
+import time
 import cv2
 import os
 import torch
@@ -5,6 +6,7 @@ import pytesseract
 import numpy as np
 from collections import Counter
 import glob
+import re
 
 # 存放辨識的dict 用於投票多數決
 def ocr_map(ocr_result, ocr_total_map):
@@ -24,11 +26,15 @@ char_to_num = {
 # 判斷貨櫃號碼 回傳 答案、狀態
 
 def is_valid_container_number(container_number):
+    container_number = re.sub(r'[^0-9A-Z]', '', container_number).replace(" ", "")
+    print(container_number)
     # 不到11
-    if len(container_number) != 11:
+    if len(container_number) < 11:
+        print('不到11')
         return 'False', False
     # 前4不為英文
     if not all(letter.isalpha() for letter in container_number[:4]):
+        print('前4不為英文')
         return 'False', False
     try:
         # 將前10個字符轉換為數值
@@ -48,8 +54,11 @@ def is_valid_container_number(container_number):
 
         # 檢查第11個字符是否等於計算出的 check_digit
         if check_digit == int(container_number[10]):
-            return container_number, True
+            return container_number[:11], True
+        print('驗證錯誤')
+        return 'False', False
     except (KeyError, ValueError):
+        print('錯誤')
         return 'False', False
 
 
@@ -84,10 +93,13 @@ def video(video_path, model):
         
         # 抓bb
         results = model(frame)
-        # 0是當前圖片 4是信心度欄位 抓最大的索引
-        f_index = results.xyxyn[0][:, 4].argmax()  
-        # 最大索引的結果
-        f_result = results.xyxyn[0][f_index]
+        if len(results.xyxyn[0]) > 0:
+            # 0是當前圖片 4是信心度欄位 抓最大的索引
+            f_index = results.xyxyn[0][:, 4].argmax()
+            # 最大索引的結果
+            f_result = results.xyxyn[0][f_index]
+        else:
+            continue
         
         width, height = frame.shape[1], frame.shape[0]
 
@@ -95,7 +107,6 @@ def video(video_path, model):
         # boundingbox
         bbox = frame[int(ymin):int(ymax), int(xmin):int(xmax)]
         CTNid = pytesseract.image_to_string(bbox)
-
         CTNid, id_stat = is_valid_container_number(CTNid)
 
         if id_stat == True:
@@ -109,7 +120,7 @@ def video(video_path, model):
         print(CTNid)
         # 在图像上绘制文本
         cv2.putText(real_frame, str(CTNid), (detections[0][1][0], detections[0][1][1]),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 3)
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3)
         cv2.rectangle(real_frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)  # BBox
             
         # 显示图像
@@ -117,20 +128,22 @@ def video(video_path, model):
             
         if cv2.waitKey(1) & 0xFF == ord('q'):  # 按下 'q' 鍵退出迴圈
             break
-        
-    max_key = max(ocr_total_map, key=ocr_total_map.get)
+    if ocr_total_map:    
+        max_key = max(ocr_total_map, key=ocr_total_map.get)
+    else:
+        max_key = ''
     print("多數決結果:" + max_key)
     cap.release()
     cv2.destroyAllWindows()
-    cv2.imwrite(f"./plate_crop/{max_key}.jpg", frame_total_map[max_key])
+    cv2.imwrite(f"./plate_crop/{max_key}.jpg", frame_total_map.get(max_key))
     return max_key
 
+start_time = time.time()
 
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best_weight/best_ex7.pt')
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Windows
-
-path = "./video"
+path = "影片資料集"
 avi_files = glob.glob(path + "/*.avi")
 answer = 0
 fail = 0
@@ -147,3 +160,8 @@ for avi_file in avi_files:
     print("anwser" + str(answer))
     print("fail" + str(fail))
     print("準確率:" + str(answer/(answer+fail)))
+# 記錄結束時間
+end_time = time.time()
+# 計算執行時間
+execution_time = end_time - start_time
+print(f"程式執行時間為 {execution_time} 秒")
